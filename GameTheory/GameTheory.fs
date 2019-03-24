f167d08120b5d2c8bbbf93da615b33c99a6fe729
ruby -v <Jbssantias@hotmail.com>
2019-03-25 01:31:03 +1000

    implemented code minimax

--- GameTheory/GameTheory.fs
+++ GameTheory/GameTheory.fs
@@ -1,58 +1,44 @@
 namespace QUT
 
     module GameTheory =
-
         let MiniMaxGenerator (heuristic:'Game -> 'Player -> int) //heuriestic score for any game situation
                              (getTurn: 'Game -> 'Player) //player's turn
                              (gameOver:'Game->bool) //Whether game is over or not
                              (moveGenerator: 'Game->seq<'Move>) //enumerate all possible moves from a given name situation
-                             (applyMove: 'Game -> 'Move -> 'Game) : 'Game -> 'Player -> Option<'Move> * int = //apply a move to a game situation to create a new game situation
+                             (applyMove: 'Game -> 'Move -> 'Game) 
+                             : 'Game -> 'Player -> Option<'Move> * int = //apply a move to a game situation to create a new game situation
             // Basic MiniMax algorithm without using alpha beta pruning
-            let rec MiniMax game perspective =
+            let rec MiniMax (game: 'Game) (perspective: 'Player) : Option<'Move> * int =
                 NodeCounter.Increment()
 
-                if gameOver game then None, 0
-                elif perspective == "Cross" then  //Cross is max and always starts the game
-                     //get player's heuristic score
-                     let score = (heuristic game perspective)
+                let rec EstablishScore (game : 'Game) (move: 'Move) (isMax: bool) : Option<'Move> * int = 
+                    if gameOver game then
+                        let score = heuristic game perspective
+                        (Some move), score
+                    else 
+                        let moves = moveGenerator game
+                        if Seq.isEmpty moves then
+                            None, 0
+                        else
+                            let nextGameStates = Seq.map (applyMove game) moves
+                            let nextGameScores = Seq.map (fun g -> EstablishScore g move (not isMax)) nextGameStates
+                            let bestMove = if isMax then Seq.maxBy snd nextGameScores else Seq.minBy snd nextGameScores
+                            bestMove
 
-                     //generate moves
-                     let moves = moveGenerator game
-
-                     //get the max value for this player
-                     let maxVal = Seq.maxBy moves
-
-                     //apply the move for the player
-                     let move = applyMove perspective maxVal
-
-                     maxVal score
-
-                else
-                    //get the player's heuristic score
-                    let score = (heuristic game perspective)
-
-                    //generate moves
-                    let moves = moveGenerator game
-
-                    //get the min value for the player
-                    let minVal = Seq.minBy moves
-                    
-                    //apply the move for the player
-                    let move = applyMove perspective minVal
-                    
-                    //return tuple containing the best move and the score associated with this game state
-
-
-                // if m is a Move
-                // then None is an Option<Move>,  or Some m is an Option<Move>
-                if gameOver then None, 0
+                let moves = moveGenerator game
+                if Seq.isEmpty moves then
+                     if gameOver game then
+                        let score = (heuristic game perspective)
+                        None, score
+                     else
+                        None, 0
                 else 
-                // maxBy : ('T -> 'U) -> seq<'T> -> 'T
-                // maxBy : ('Move -> 'U) -> seq<'Move> -> 'Move
-                    //let score (move : 'Move) = (heuristic game perspective)
-                    
-
-                raise (System.NotImplementedException("MiniMax")) //remove line
+                    let possibilities = Seq.map (fun m -> (m, applyMove game m)) moves
+                    let endGames = Seq.map (fun (m, g) -> EstablishScore g m true) possibilities
+                    let bestMove = Seq.maxBy snd endGames
+                    bestMove
+               
+                //raise (System.NotImplementedException("MiniMax")) //remove line
 
             NodeCounter.Reset()
             MiniMax
